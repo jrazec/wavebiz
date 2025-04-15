a0bc6ea2011a5f02687aafac65fe47ed1243e68f
jrazec <johnrazeagno@gmail.com>
2025-04-15 16:34:45 +0800

    Create migrations

--- database/migrations/2025_04_15_071319_create_password_reset_tokens_table.php
+++ database/migrations/2025_04_15_071319_create_password_reset_tokens_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::connection('wb_db')->create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
